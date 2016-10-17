4bc92acde6002b878d2444d8e77e9f1b84b13c73
desteny21 <im6h@fresnou.org>
2016-10-17 19:53:30 +0000

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+var header = document.getElementById("header");
+
+header.style = "background:#e6f2ff; border:1px solid white;";
+
+var title = document.createElement("h1");
+title.textContent = "Random Tip Generator Fam!";
+header.appendChild(title);
+var description = document.createElement("p");
+description.innerHTML = "This is the description of the project G"
+header.appendChild(description);
